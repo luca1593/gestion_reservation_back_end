b163f9d915de007c1274cc406590611ed1be15ca
Luca Adam <luca.adam23@gmail.com>
2025-09-24 15:04:19 +0300

    accessibiliter applicative

--- src/main/java/com/detech/gsrt/GsrtApplication.java
+++ src/main/java/com/detech/gsrt/GsrtApplication.java
@@ -2,6 +2,7 @@ package com.detech.gsrt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class GsrtApplication extends SpringBootServletInitializer {
+
+    @Override
+    public SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder){
+        return applicationBuilder.sources(GsrtApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(GsrtApplication.class, args);
---
+++ src/main/java/com/detech/gsrt/controller/api/HomeControleur.java
@@ -0,0 +1,14 @@
+package com.detech.gsrt.controller.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeControleur {
+    @GetMapping
+    public String home(){
+        return  "Bienvenu sur GSRT API";
+    }
+}
