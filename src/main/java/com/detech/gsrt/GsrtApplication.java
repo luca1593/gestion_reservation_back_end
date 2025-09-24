bc6537f90f3d254e4de85e23eff6219467df2eff
Luca Adam <luca.adam23@gmail.com>
2025-09-24 13:27:02 +0300

    modification contextpath et ajout nom d'affichage

--- src/main/java/com/detech/gsrt/GsrtApplication.java
+++ src/main/java/com/detech/gsrt/GsrtApplication.java
@@ -2,15 +2,16 @@ package com.detech.gsrt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-@EnableJpaRepositories
-@EnableJpaAuditing
+@Configuration
 @ComponentScan
-public class GsrtApplication {
+@SpringBootApplication
+@EnableJpaAuditing
+public class GsrtApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GsrtApplication.class, args);
